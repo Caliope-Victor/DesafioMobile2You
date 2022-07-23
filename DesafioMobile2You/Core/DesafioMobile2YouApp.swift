a0fafedcf8a50f08f10dd32d0a7e15b80e2a5638
Caliope-Victor <72174923+Caliope-Victor@users.noreply.github.com>
2022-07-23 15:12:02 -0300

    first commit

---
+++ DesafioMobile2You/Core/DesafioMobile2YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioMobile2YouApp.swift
+//  DesafioMobile2You
+//
+//  Created by Victor Henrique Caliope Soares on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DesafioMobile2YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieDetailsView()
+        }
+    }
+}
---
+++ DesafioMobile2You/View/MovieDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesafioMobile2You
+//
+//  Created by Victor Henrique Caliope Soares on 22/07/22.
+//
+
+import SwiftUI
+
+struct MovieDetailsView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailsView()
+    }
+}
